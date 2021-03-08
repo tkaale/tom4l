e09fef3c1444c40bd66df4cd30289c3aac525d6a
Aleksandra <tkaczyk.aleksandra@outlook.com>
2021-03-08 02:32:34 +0100

    first commit / added nav

---
+++ javaScript/script.js
@@ -0,0 +1,8 @@
+const navBtn = document.querySelector('.nav__mobile-btn');
+const nav = document.querySelector('.nav__mobile-list');
+
+function menuStart() {
+    nav.classList.toggle('nav__mobile-list--active')
+}
+
+navBtn.addEventListener('click', menuStart);
