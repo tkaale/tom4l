89aa0991ed602111f3e5e6d208f8219b239ab4d6
Aleksandra <tkaczyk.aleksandra@outlook.com>
2021-03-10 03:20:52 +0100

    section aboutus-second

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,17 @@
 const navBtn = document.querySelector('.nav__mobile-bars-btn');
 const nav = document.querySelector('.nav__mobile-list');
+const navItem = document.querySelectorAll('.nav__mobile-list-item');
 
-function menuStart() {
-    nav.classList.toggle('nav__mobile-list--active')
-}
+
+const menuStart = () => {
+    nav.classList.toggle('nav__mobile-list--active');
+
+    navItem.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav__mobile-list--active');
+        });
+    });
+
+};
 
 navBtn.addEventListener('click', menuStart);
