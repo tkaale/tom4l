70b23c53077561fa129bc8664c3774ef65909861
Aleksandra <tkaczyk.aleksandra@outlook.com>
2021-04-08 19:18:28 +0200

    commit

--- javaScript/script.js
+++ javaScript/script.js
@@ -15,3 +15,31 @@ const menuStart = () => {
 };
 
 navBtn.addEventListener('click', menuStart);
+
+
+
+
+const navShadow = document.querySelector('.nav');
+const navShadowIco = document.querySelector('.nav__brand-ico');
+const navShadowBtn = document.querySelector('.nav__mobile-bars-btn');
+const navShadowLink = document.querySelectorAll('.nav__desktop-list-item');
+
+function addShadow() {
+    if (window.scrollY >= 200) {
+        navShadow.classList.add('shadow-bg');
+        navShadowIco.classList.add('color');
+        navShadowBtn.classList.add('color');
+        for (let i = 0; i < navShadowLink.length; i++) {
+            navShadowLink[i].classList.add('color');
+        }
+
+    } else {
+        navShadow.classList.remove('shadow-bg');
+        navShadowIco.classList.remove('color');
+        navShadowBtn.classList.remove('color');
+        for (let i = 0; i < navShadowLink.length; i++) {
+            navShadowLink[i].classList.remove('color');
+        }
+    }
+}
+window.addEventListener('scroll', addShadow)
